4d32a2a8cfe983e01e87e669468de1d6725f680d
sssloba <slobodan.stojakov@gmail.com>
2024-01-25 23:49:06 +0100

    firebase-options-config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:remind_me/api/firebase_api.dart';
+import 'package:remind_me/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseApi().initNotification();
   runApp(const MyApp());
 }
