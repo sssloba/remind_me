7d0b307dff002717d73a99b33e022b11de97eb08
sssloba <slobodan.stojakov@gmail.com>
2024-01-28 01:15:57 +0100

    background-message-handling

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -2,12 +2,19 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:remind_me/congif/config.dart';
 
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  debugPrint('Title : ${message.notification?.title}');
+  debugPrint('Body: ${message.notification?.body}');
+  debugPrint('Payload: ${message.data}');
+}
+
 class FirebaseApi {
   final FirebaseMessaging _firebaseMessagin = FirebaseMessaging.instance;
 
   Future<void> initNotification() async {
     await _firebaseMessagin.requestPermission();
     Config.fcmToken = await _firebaseMessagin.getToken();
-    debugPrint('Token ${Config.fcmToken}');
+    debugPrint('Token: ${Config.fcmToken}');
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 }
